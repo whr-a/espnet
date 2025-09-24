2aab488f0ddcf609a834ea8e98c00788d1befab3
Haoran Wang <2472356642@qq.com>
2025-09-23 23:18:08 -0500

    update

---
+++ egs2/lrac/track2/run_arecho.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+fs=24000
+
+opts="--audio_format wav "
+
+train_set=speech
+valid_set=dev_small
+test_sets="open_testset_track2_clean open_testset_track2_noisy open_testset_track2_reverb"
+# test_sets="test_all"
+train_config=conf/codec_arecho/arecho_v1.yaml
+inference_config=conf/decode.yaml
+score_config=conf/score.yaml
+
+./codec.sh \
+    --local_data_opts "--trim_all_silence false" \
+    --fs ${fs} \
+    --train_config "${train_config}" \
+    --inference_config "${inference_config}" \
+    --scoring_config "${score_config}" \
+    --stage 5 \
+    --stop_stage 5\
+    --ngpu 1 \
+    --nj 12 \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    ${opts} "$@"
